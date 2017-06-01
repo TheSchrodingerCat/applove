1994678e3c4a7581507b743c81efe0c17a98f026
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-01 12:10:13 -0400

    se eliminan textos del modal, se arregla el footer y se arreglan los espacion entre lines de 'Services'

--- js/app.js
+++ js/app.js
@@ -10,25 +10,31 @@
  	var bodyModal, close, img;
  		boxes.forEach(function(box){
  			box.addEventListener("click", function(){
- 			modal.innerHTML = "";
- 			bodyModal = document.createElement("div");
- 			bodyModal.classList.add("modal-body");
- 			bodyModal.innerHTML = box.innerHTML;
- 			modal.appendChild(bodyModal);
- 			modal.classList.remove("hide");
-			close = document.createElement("div");
-			close.classList.add("close");
 
-			var text = document.getElementById("texto" + boxes.indexOf(box).toString());
-			text.classList.toggle("hide");
+ 				modal.innerHTML = ""; //si no lo pongo no se ve el botón close....
 
-			img = document.createElement("img");
-			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
-			close.appendChild(img);
-			modal.appendChild(close);
-			close.addEventListener("click",function(){
-			modal.classList.add("hide");
-			text.classList.toggle("hide");
+ 				var text = document.getElementById("texto" + boxes.indexOf(box).toString());
+				text.classList.toggle("hide");
+
+				//tamaño de imagen en modal (no resulta)
+				var img = document.getElementById("images" + boxes.indexOf(box).toString());
+				img.classList.add("big-image");
+
+ 				bodyModal = document.createElement("div");
+ 				bodyModal.classList.add("modal-body");
+ 				bodyModal.innerHTML = box.innerHTML;
+ 				modal.appendChild(bodyModal);
+ 				modal.classList.remove("hide");
+				close = document.createElement("div");
+				close.classList.add("close");
+
+				img = document.createElement("img");
+				img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+				close.appendChild(img);
+				modal.appendChild(close);
+				close.addEventListener("click",function(){
+				modal.classList.add("hide");
+				text.classList.toggle("hide");
 			});
 		})
  	});
