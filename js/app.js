1e3dfe5eef97bf319d1e0527b03da346c5268f4f
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-01 11:02:55 -0400

    modal casi listo, faltan eliminar los textos del modal y agrandar las fotos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+ (function main(){
+ 	var boxes = Array.from(document.getElementsByClassName("cajita"));
+ 	var modal = document.getElementById("box-services-modal");
+ 	
+
+ 	var bodyModal, close, img;
+ 		boxes.forEach(function(box){
+ 			box.addEventListener("click", function(){
+ 			modal.innerHTML = "";
+ 			bodyModal = document.createElement("div");
+ 			bodyModal.classList.add("modal-body");
+ 			bodyModal.innerHTML = box.innerHTML;
+ 			modal.appendChild(bodyModal);
+ 			modal.classList.remove("hide");
+			close = document.createElement("div");
+			close.classList.add("close");
+
+			var text = document.getElementById("texto" + boxes.indexOf(box).toString());
+			text.classList.toggle("hide");
+
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+			modal.classList.add("hide");
+			text.classList.toggle("hide");
+			});
+		})
+ 	});
+ })();
